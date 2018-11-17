8f7a9ddada63d2b9d283ec26ae61844aedb65e39
wen <360601917@qq.com>
2018-11-17 22:37:26 +0800

    a

--- application/api/controller/v1/Banner.php
+++ application/api/controller/v1/Banner.php
@@ -2,13 +2,19 @@
 namespace app\api\controller\v1;
 
 use app\api\validate\IDMustBePostivelint;
+use app\api\model\Banner as BannerModel;
+use think\Exception;
 
 class Banner {
   //put your code here
   public function getBanner($id){
 
     (new IDMustBePostivelint())->goCheck();
-    $a=$validate->goch;
-    dump($a);
+    $banner= BannerModel::getBannerById($id);
+    if(!$banner){
+      throw new Exception('wwwwwwwwwww');
+      throw new \app\lib\exception\BannerMissException();
+    }
+    return $banner;
   }
 }
---
+++ application/api/model/Banner.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\api\model;
+
+use think\Exception;
+
+class Banner {
+  static function getBannerById($id){
+    return null;
+  }
+}
